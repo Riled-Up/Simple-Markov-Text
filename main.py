91626cfb9fa19208f28b73964c4a80bd0594973b
Riley Guy <rileygriffinguy@gmail.com>
2017-09-11 17:16:31 -0400

    Added "archives" directory

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import pickle
 
 
 def save_obj(obj, name ):
-    with open('obj/'+ name + '.pkl', 'wb') as f:
+    with open('archives/default/'+ name + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 def load_obj(name):
-    with open('obj/' + name + '.pkl', 'rb') as f:
+    with open('archives/default/' + name + '.pkl', 'rb') as f:
         return pickle.load(f)
 
 
@@ -65,8 +65,8 @@ class MarkovChainWindow:
     
     def add_text_to_archive(self):
         new_word_frequency, new_list_of_dicts = markov.read_text(self.input_text.get(1.0, "end-1c"))
-        old_word_frequency = load_obj(word_frequency)
-        old_list_of_dicts = load_obj(list_of_dicts)
+        old_word_frequency = load_obj("word_frequency")
+        old_list_of_dicts = load_obj("list_of_dicts")
         for curr_new_dict in new_list_of_dicts:
             for curr_old_dict in old_list_of_dicts:
                 if curr_old_dict['Preceding Word:'] == curr_new_dict['Preceding Word:']:
