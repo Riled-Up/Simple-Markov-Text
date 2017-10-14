2e6ce302c125559c10c376bbe1e8a9c0591a1d6b
Unknown <rileygriffinguy@gmail.com>
2017-10-14 16:27:21 -0400

    More documentation

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import pickle
 import os
 
 class MarkovChainWindow:
+    """Contains all the widgets and methods for the program."""
     def __init__(self, root):
+        """Creates widgets, packs them, and initializes the archive list."""
         root.title("Markov Chain")
         # Makes widgets.
         self.top_frame = tk.Frame(root)
@@ -21,15 +23,15 @@ class MarkovChainWindow:
         self.archive_current_frame = tk.Frame(self.archive_frame) 
         self.archive_current_label = tk.Label(self.archive_current_frame, text = "Current Archive:") 
         self.archive_current = tk.Entry(self.archive_current_frame)
-        self.archive_current.bind("<Key>", self.archive_entry_changed)
+        self.archive_current.bind("<Key>", self.archive_entry_changed) # Calls function when user types in field.
         self.archive_list_frame = tk.Frame(self.archive_frame)
         self.archive_list_scrollbar = tk.Scrollbar(self.archive_list_frame)
         self.archive_list = tk.Listbox(self.archive_list_frame, yscrollcommand = self.archive_list_scrollbar.set, selectmode = 'single')
-        self.archive_list.bind("<<ListboxSelect>>", self.archive_list_clicked)
+        self.archive_list.bind("<<ListboxSelect>>", self.archive_list_clicked) # Calls function when user selects item.
         self.archive_list_scrollbar.config(command = self.archive_list.yview)
         self.update_archive_list()
-        self.archive_list.activate('anchor')
-        self.archive_current.insert(0, self.archive_list.get('active'))
+        self.archive_list.activate('anchor') # Activates last item in archive_list.
+        self.archive_current.insert(0, self.archive_list.get('active')) # Puts name of selected archive in archive_current.
         self.button_horizontal_frame = tk.Frame(self.archive_frame)
         self.button_new_archive = tk.Button(self.button_horizontal_frame, text = "New", command = self.new_archive)
         self.button_delete_archive = tk.Button(self.button_horizontal_frame, text = "Delete", command = self.delete_archive)
@@ -57,50 +59,58 @@ class MarkovChainWindow:
         self.input_text.pack(fill = "both", expand = True)
     
     def save_obj(self, obj, name):
-        with open('archives/'+ name + '.pkl', 'wb') as f:
+        """Saves .pkl objects."""
+        with open('archives/'+ name + '.pkl', 'wb') as f: # Saves it in archive directory
             pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
     
     def load_obj(self, name):
-        with open('archives/' + name + '.pkl', 'rb') as f:
+        """Loads .pkl objects."""
+        with open('archives/' + name + '.pkl', 'rb') as f: # Opens it in archive directory
             return pickle.load(f)
     
     def archive_list_clicked(self, event):
-        if self.archive_list.curselection() == ():
+        """Called when user selects something in the listbox."""
+        if self.archive_list.curselection() == (): # Returns 1 if nothing in archive_list is selected.
             return 1
-        self.archive_current.delete(0, 'end')
-        self.archive_current.insert(0, self.archive_list.get(self.archive_list.curselection()))
+        self.archive_current.delete(0, 'end') # Clears archive_current.
+        self.archive_current.insert(0, self.archive_list.get(self.archive_list.curselection())) # Inserts title of selected archive.
     
     def archive_entry_changed(self, event):
+        """Checks to see if anything in archive_list matches user input. Activates the input if input matches."""
         self.update_archive_list()
-        index = 0
-        for archive in self.archive_list.get(0, 'end'):
-            if archive == self.archive_current.get() + event.char:
+        index = 0 # Tracks position in archive_list.
+        for archive in self.archive_list.get(0, 'end'): # Searches through name of items.
+            if archive == self.archive_current.get() + event.char: # Activates if name matches user input.
                 self.archive_list.activate(index)
-            else:
+            else: # Increments index if name of item did not match.
                 index += 1
     
     def update_archive_list(self):
-        self.archive_list.delete(0, 'end')
-        for archive in os.listdir('archives/'):
+        """Updates the items in the archive_list. Useful if user puts in .pkl file in archives directory manually."""
+        self.archive_list.delete(0, 'end') # Deletes all items 
+        for archive in os.listdir('archives/'): # Inserts any files with .pkl extension into archive_list.
             if archive[-4:] == '.pkl':
                 self.archive_list.insert('end', archive[:-4])
                 
     def new_archive(self):
+        """Adds blank archive to archives directory."""
         self.update_archive_list()
-        if self.archive_current.get() == '':
+        if self.archive_current.get() == '': # Checks if archive_current is blank.
             tkMessageBox.showwarning("Nothing entered", "You didn't type anything in the text box.")
             return 1
         for archive in os.listdir('archives/'):
-            if archive[:-4] == self.archive_current.get():
+            if archive[:-4] == self.archive_current.get(): # Checks if archive already exists.
                 tkMessageBox.showwarning("Archive already exists", "The archive '%s' already exists." % archive[:-4])
                 return 1
-        self.save_obj([], self.archive_current.get())
-        self.archive_list.insert('end', self.archive_current.get())
-        self.archive_list.activate('end')
+        self.save_obj([], self.archive_current.get()) # Saves blank archive.
+        self.archive_list.insert('end', self.archive_current.get()) # Inserts into archive_list.
+        self.archive_list.activate('end') # Activates it in archive_list.
         
     
     def delete_archive(self):
+        """Deletes archive in archives directory."""
         archive_exists = False
+        # Reaffirms user decision and checks if archive exists.
         for archive in os.listdir('archives/'):
             if archive[:-4] == self.archive_current.get():
                 if tkMessageBox.askyesno("Archive deletion", "Are you sure you want to delete the '%s' archive?" % self.archive_current.get()):
@@ -108,14 +118,16 @@ class MarkovChainWindow:
                 archive_exists = True
         if archive_exists == False:
             tkMessageBox.showwarning("No archive named '%s'" % self.archive_current.get(), "There is currently no archive named '%s'." % self.archive_current.get())
-        self.archive_list.activate('end')
+        self.archive_list.activate('end') # Activates last item in archive_list.
         self.update_archive_list()
-        self.archive_current.delete(0, 'end')
-        self.archive_current.insert(0, self.archive_list.get('end'))
+        self.archive_current.delete(0, 'end') # Deletes text inside archive_current.
+        self.archive_current.insert(0, self.archive_list.get('end')) # Inserts name of last archive in archive_list.
     
     
     def add_text_to_archive(self):
+        """Adds data to archive."""
         self.update_archive_list()
+        # Checks if an archive is selected and if the archive exists.
         if self.archive_current.get() == '':
             tkMessageBox.showwarning("No archive selected", "Please select an archive.")
             return 1
@@ -126,27 +138,30 @@ class MarkovChainWindow:
         if pickle_file_exists == False:
             tkMessageBox.showwarning("No archive named '%s'" % self.archive_current.get(), "There is currently no archive named '%s'." % self.archive_current.get())
             return 1
-        new_list_of_dicts = markov.read_text(self.input_text.get(1.0, "end-1c"))
-        old_list_of_dicts = self.load_obj(self.archive_current.get())
+        new_list_of_dicts = markov.read_text(self.input_text.get(1.0, "end-1c")) # Creates new archive from input_text.
+        old_list_of_dicts = self.load_obj(self.archive_current.get()) # Gets old archive.
+        # Merges the old archive with the new one.
         should_add = True
-        for curr_old_dict in old_list_of_dicts:
+        for curr_old_dict in old_list_of_dicts: # Sorts through old archive to find matching preceding words in the new one.
             for curr_new_dict in new_list_of_dicts:
                 if curr_old_dict['Preceding Word'] == curr_new_dict['Preceding Word']:
                     should_add = False
                     for old_word in curr_old_dict:
-                        if curr_new_dict.has_key(old_word) and old_word != 'Preceding Word':
+                        if curr_new_dict.has_key(old_word) and old_word != 'Preceding Word': # Adds total to word if they match.
                             curr_new_dict[old_word] += curr_old_dict[old_word]
-                        else:
+                        else: # Otherwise, add the word to the dictionary.
                             curr_new_dict[old_word] = curr_old_dict[old_word]
-            if should_add:
+            if should_add: # Adds dictionary to the new archive if an identical preceding word is not found.
                 new_list_of_dicts.append(curr_old_dict)
             should_add = True
-        self.save_obj(new_list_of_dicts, self.archive_current.get())
-        self.input_text.delete('1.0', 'end')
+        self.save_obj(new_list_of_dicts, self.archive_current.get()) # Saves the merged archive.
+        self.input_text.delete('1.0', 'end') # Clears input.
 
 
     def generate_output(self):
+        """Generates text from archive."""
         self.update_archive_list()
+        # Checks if current archive is blank and if it exists.
         if self.archive_current.get() == '':
             tkMessageBox.showwarning("No archive selected", "Please select an archive.")
             return 1
@@ -157,11 +172,11 @@ class MarkovChainWindow:
         if pickle_file_exists == False:
             tkMessageBox.showwarning("No archive named '%s'" % self.archive_current.get(), "There is currently no archive named '%s'." % self.archive_current.get())
             return 1
-        self.output_text.delete('1.0', 'end')
-        self.output_text.insert('1.0', markov.generate_markov_chain(self.load_obj(self.archive_current.get())))
+        self.output_text.delete('1.0', 'end') # Clears output_text.
+        self.output_text.insert('1.0', markov.generate_markov_chain(self.load_obj(self.archive_current.get()))) # Inserts whatever generate_markov_chain returns.
 
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    window = MarkovChainWindow(root)
-    root.mainloop()
+if __name__ == "__main__": # Only perform if this file is being run directly.
+    root = tk.Tk() # Creates window.
+    MarkovChainWindow(root) # Initializes the window.
+    root.mainloop() # Enters the main loop of the window.
--- markov.py
+++ markov.py
@@ -4,23 +4,23 @@ from random import randint
 
 
 def read_text(text):
-    list_of_words = text.split()
-    list_of_dicts = []
-    list_of_dicts.append({'Preceding Word' : ''})
+    """Generates archive based on text."""
+    list_of_words = text.split() # Splits text up into individual words.
+    list_of_dicts = [{'Preceding Word' : ''}] # Makes the blank preceding word.
     preceding_word = ''
     should_add = True
-    for word in list_of_words:
-        for curr_dict in list_of_dicts:
+    for word in list_of_words: # Sorts through every word in the input text.
+        for curr_dict in list_of_dicts: # Searches for a matching preceding word in all the available dictionaries.
             if curr_dict['Preceding Word'] == preceding_word:
-                if curr_dict.has_key(word):
+                if curr_dict.has_key(word): # Adds 1 to value of word in dictionary if it exists.
                     curr_dict[word] += 1
-                else:
+                else: # Otherwise, create it with a value of 1.
                     curr_dict[word] = 1
-            if curr_dict['Preceding Word'] == word:
+            if curr_dict['Preceding Word'] == word: # Don't add a dictionary for the current word if one already exists.
                 should_add = False
-        if should_add == True:
+        if should_add == True: # If the word did not have a dictionary for it, make one.
             list_of_dicts.append({'Preceding Word' : word})
-        for char in word:
+        for char in word: # Make the preceding word blank if the word had punctuation in it.
             if char == '.' or char == '?' or char == '!':
                 word = ''
         preceding_word = word
@@ -29,13 +29,14 @@ def read_text(text):
 
 
 def _pick_from_dict(dictionary):
+    """Randomly picks key from dictionary based on its value."""
     sum_of_ints = 0
-    for key in dictionary:
+    for key in dictionary: # Adds value of every key in dictionary together.
         if type(dictionary[key]) is int:
             sum_of_ints += dictionary[key]
-    r = randint(0, sum_of_ints - 1)
+    r = randint(0, sum_of_ints - 1) # Makes a random number in between 0 and sum_of_ints.
     sum_of_ints = 0
-    for key in dictionary:
+    for key in dictionary: # I can't really explain this, but it works.
         if type(dictionary[key]) is int:
             sum_of_ints += dictionary[key]
             if r < sum_of_ints:
@@ -43,18 +44,19 @@ def _pick_from_dict(dictionary):
 
 
 def generate_markov_chain(list_of_dicts):
-    output = '' 
+    """Generates archive based on text."""
+    output = ''
     words_placed = 0
     preceding_word = ''
-    while words_placed <= 100:
-        for item in list_of_dicts:
+    while words_placed <= 100: # Keep going until 100 words have been placed.
+        for item in list_of_dicts: # Search for a matching preceding word in the archive.
             if item['Preceding Word'] == preceding_word:
                 if len(item) != 1:
                     preceding_word = _pick_from_dict(item)
                     output += preceding_word + ' '
                     words_placed += 1
-                else:
+                else: # If there are no other options, set preceding word to blank.
                     preceding_word = ''
-        if words_placed == 0:
+        if words_placed == 0: # Prevents any infinite loops.
             return output
     return output
